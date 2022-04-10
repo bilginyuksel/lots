174497ce646805f1a5cf51cca16e8beea5b946b1
bilginyuksel <bilgin.yuksel96@gmail.com>
2022-04-10 23:36:20 +0300

    Add transfer container

---
+++ src/component/transfer/Transfer.tsx
@@ -0,0 +1,9 @@
+import { Card, CardHeader } from "@mui/material";
+
+export default function TransferContainer() {
+    return (
+        <Card className="card">
+            <CardHeader title="Transfer Funds" />
+        </Card>
+    )
+}
